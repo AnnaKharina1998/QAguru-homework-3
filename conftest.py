6a8954f38110245ab8a88fbd9b184f6497c6e448
Анна Краснокутская <anna.kharina.1998@mail.ru>
2024-06-26 09:26:48 +0300

    Исправлены в соответствии с комментариями фикстуры и тесты на поиск в гугле, удалены тесты на поиск в яндексе и duckduckgo, поскольку их не было в задании, а возни с ними много

--- conftest.py
+++ conftest.py
@@ -2,27 +2,31 @@ import pytest
 from selene import browser
 
 
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope="function", autouse=True)
 def browser_options():
     browser.config.window_width = '1920'
     browser.config.window_height = '1080'
+    browser.config.base_url = 'https://google.com/'
 
-
-@pytest.fixture(scope="function")
-def google_search():
-    browser.open('https://google.com')
     yield
     browser.quit()
 
-@pytest.fixture(scope="function")
-def yandex_search():
-    browser.open('https://ya.ru/')
-    yield
-    browser.quit()
 
-@pytest.fixture(scope="function")
-def duck_search():
-    browser.open('https://duckduckgo.com')
-    yield
-    browser.quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- test_find_or_not_find.py
+++ test_find_or_not_find.py
@@ -1,20 +1,15 @@
-from selene import browser, have, by
-import pytest
+from selene import browser, have
 
-def test_google_should_find_selene(google_search):
+
+def test_google_should_find_selene():
+    browser.open('')
     browser.element('[name="q"]').type('yashaka/selene').press_enter()
-    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+    browser.element('#search').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
-def test_google_should_not_find(google_search):
+def test_google_should_not_find():
+    browser.open('')
     browser.element('[name="q"]').type(';ko/l.ik,gmjhndgbfvdcavzsbfxgn,vjhl.b/kn;ml/.k,jbmhvngcfbx').press_enter()
-    browser.element('[id="center_col"]').should(have.text('ничего не найдено'))
+    browser.element('#center_col').should(have.text("По запросу ;ko/l.ik,gmjhndgbfvdcavzsbfxgn,vjhl.b/kn;ml/.k,jbmhvngcfbx ничего не найдено"))
 
 
-def test_yandex_should_find_selene(yandex_search):
-    browser.element(by.xpath("//input[@placeholder='Найдётся всё']")).type('yashaka/selene').press_enter()
-    browser.element(by.xpath("//ul[@id='search-result']")).should(have.text('yashaka/selene'))
-
-def test_duck_should_find_selene(duck_search):
-    browser.element(by.xpath("//input[@placeholder='Поиск без отслеживания']")).type('yashaka/selene').press_enter()
-    browser.element(by.xpath("//ol[@class='react-results--main']")).should(have.text('yashaka/selene'))
 
